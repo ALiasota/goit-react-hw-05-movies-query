e7bc72fa2f2ee47d73d9f1e2b6ffaa3c7023e674
Andrii Liasota <lyasota.andriy@gmail.com>
2022-05-25 21:43:35 +0300

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React, { lazy, Suspense } from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './Navigation';
+
+const HomePage = lazy(()=> import('./HomePage'));
+const MoviesPage = lazy(() => import('./MoviesPage'));
+const MovieDetailsPage = lazy(()=> import('./MovieDetailsPage'));
+const Nothing = lazy(()=>import('./Nothihg'));
+
+const queryClient = new QueryClient();
+const App = () => {
+  
+  return (      
+    <QueryClientProvider client = { queryClient }>
+    <Suspense fallback={<h1>Loading</h1>}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>  
+            <Route index element={<HomePage />} />        
+            <Route path="movies" element={<MoviesPage />} />
+            <Route path="movies/:movieId/*" element={<MovieDetailsPage />} />
+            <Route path='*' element={<Nothing />} />
+        </Route>    
+      </Routes>
+    </Suspense>
+    </QueryClientProvider>    
+  );
+};
+
+export default App;
